c08bcf827da1b99260bb606a75ec5b0ed02b271a
Merllin Ann George <47381713+merllinanngeorge@users.noreply.github.com>
2025-10-01 23:24:04 +0530

    Time: 13 ms (40.89%) | Memory: 45.2 MB (23.16%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i = 0;
+        int j = str.length()-1;
+        while(i<=j)
+        {
+            if(str.charAt(i)!=str.charAt(j))
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
