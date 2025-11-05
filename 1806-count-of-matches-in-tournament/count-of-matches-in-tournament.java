5e4cb08360fb0c5e0dda471603d9a27a8d0874a3
Merllin Ann George <47381713+merllinanngeorge@users.noreply.github.com>
2025-11-05 19:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (7.21%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int count = 0;
+        while(n>1)
+        {
+            if(n%2==0)
+            count += n/2;
+            else
+            count += ((n-1)/2 + 1);
+            n /= 2;
+        }
+        return count;
+    }
+}
