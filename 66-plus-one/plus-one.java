4876a3eadf0f664a8f8ea47f7274c0a1a7357950
Merllin Ann George <47381713+merllinanngeorge@users.noreply.github.com>
2025-12-13 07:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (21.27%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for(int i=n-1; i>=0; i--)
+        for(int i=digits.length-1; i>=0; i--)
         {
-            if(digits[i]<9)
+            if(digits[i] < 9)
             {
                 digits[i]++;
                 return digits;
             }
             else
-            digits[i] = 0;
+            {
+                digits[i] = 0;
+            }
         }
-        int newArray[] = new int[n+1];
+        int newArray[] = new int[digits.length+1];
         newArray[0] = 1;
         return newArray;
     }
